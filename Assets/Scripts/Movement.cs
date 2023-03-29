fdd38b93f9735dbb0342227b5942b3f562105878
zirris512 <zirris512@gmail.com>
2023-03-28 21:26:13 -0500

    initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace RPG.Character
+{
+    public class Movement : MonoBehaviour
+    {
+        public void HandleMove()
+        {
+            print("WASD Keys pressed!");
+        }
+    }
+}
